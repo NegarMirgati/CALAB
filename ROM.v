e8240fd765a57123a07e5f8416b749b34bc543ed
Nazanin Sabri <nazanin.sabrii@gmail.com>
2018-10-10 15:16:31 +0330

    connect the sevent segment converter and rom initial values

--- ROM.v
+++ ROM.v
@@ -2,6 +2,11 @@ module ROM(input clock, input [31:0] address, output [32:0] instruction);
 
     reg [10:0] rom [31:0]; 
 
+    initial begin 
+        for(i=0; i<1023; i = i+1) 
+            rom[i] = 16'd10;
+    end 
+
     always @ (posedge clock)
     begin 
         instruction = rom[address];
--- pipeline.v
+++ pipeline.v
@@ -8,6 +8,12 @@ module pipeline(input clock, input reset, output [LEN-1:0] ifid_pc_output, outpu
     wire [LEN-1:0] exemem_pc_out, exemem_instruction_out;
     wire [LEN-1:0] memwb_pc_out, memwb_instruction_out;
 
+    wire [6:0] ss_ifid_pc;
+    wire [6:0] ss_idexe_pc;
+    wire [6:0] ss_exemem_pc;
+    wire [6:0] ss_memwb_pc;
+
+
     IF instFetch(clock, reset, instruction, pc); 
     
     IFID ifidreg #(LEN) (clock, reset, pc, instruction, ifid_pc_out, ifid_instruction_out);
@@ -19,5 +25,10 @@ module pipeline(input clock, input reset, output [LEN-1:0] ifid_pc_output, outpu
     assign idexe_pc_output = idexe_pc_out;
     assign exemem_pc_output = exemem_pc_out;
     assign memwb_pc_output = memwb_pc_out;
+
+    SevenSegmentConverter sscIFID(ifid_pc_out[5:2], ss_ifid_pc);
+    SevenSegmentConverter sscIDEXE(idexe_pc_out[5:2], ss_idexe_pc);
+    SevenSegmentConverter sscEXEMEM(exemem_pc_out[5:2], ss_exemem_pc);
+    SevenSegmentConverter sscMEMWB(memwb_pc_out[5:2], ss_memwb_pc);
 
 endmodule
