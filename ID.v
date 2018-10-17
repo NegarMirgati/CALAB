b296a358d9e6899279eeef53a9dcb243f68f8b91
Nazanin Sabri <nazanin.sabrii@gmail.com>
2018-10-17 13:51:39 +0330

    completed mux and minor changes to ID

--- ID.v
+++ ID.v
@@ -1,9 +1,22 @@
 module ID (
 	input clock, 
 	input reset, 
-	input [31:0] instruction
+	input [31:0] instruction, 
+	input [31:0] PC, 
+	output exe_cmd, 
+	ouput mem_write, 
+	ouput mem_read, 
+	output br_type, 
+	output writeback_EN,
 );
-
 	
+	Controller cUnit (instruction[31:26], );
+
+	Register_file reg_file(clock, reset, instruction[25:21], instruction[20:16], instruction[15:11]);
+
+	SignExtend se ();
+
+	MUX  ();
+	MUX dest_mux ();
 
 endmodule
--- MUX.v
+++ MUX.v
@@ -1,5 +1,5 @@
-module MUX  #(parameter LEN = 32) (
+module MUX  #(parameter LEN = 32) ( input [LEN-1:0] value1, input [LEN-1:0] value2, input selector, output [LEN-1:0] out_val
 );
-
+	assign out_val = (selector)? value1 : value2;
 
 endmodule 
