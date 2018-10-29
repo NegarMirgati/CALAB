d579720c9da9da141ba3b6f192a44f79eb15732a
Nazanin Sabri <nazanin.sabrii@gmail.com>
2018-10-29 07:54:57 +0330

    add writeback

---
+++ WB.v
@@ -0,0 +1,10 @@
+module WB(
+	input mem_r_en, 
+	input [31:0] alu_result, 
+	input [31:0] memory_result, 
+	output [31:0] result_wb
+);
+
+	MUX wb_m(.value1(memory_result), .value2(alu_result),  .selector(mem_r_en), .out_val(result_wb));
+
+endmodule
