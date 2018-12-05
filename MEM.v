60e1b8c77ffdf10f82b5941f4209eeb1ed762ce2
Nazanin Sabri <nazanin.sabrii@gmail.com>
2018-12-05 13:49:39 +0330

    trying to implement sram controller

--- MEM.v
+++ MEM.v
@@ -14,4 +14,7 @@ module MEM(
 
 	DataMemory dm (.clock(clock), .reset(reset), .address(address), .write_data(ST_value), .mem_r_en(mem_r_en), .mem_w_en(mem_w_en), .data(memory_result));
 
+
+	Sram_Controller sram(.clk(clock), .rst(reset), .wr_en(mem_w_en), .rd_en(mem_r_en), .address(address), .write_data(ST_value) , .read_data(memory_result), .ready() , .SRAM_DQ(), .SRAM_ADDR(), .SRAM_UB_N(), .SRAM_LB_N(), .SRAM_WE_N(), .SRAM_CE_N(), .SRAM_OE_N() );
+
 endmodule
--- Sram_Controller.v
+++ Sram_Controller.v
@@ -16,5 +16,26 @@ module Sram_Controller(
 	output SRAM_OE_N 
 	);
 
+	wire cycle = 1'b0; 
+	always @(clk) begin 
+		cycle = cycle + 1'b1; 
+	end 
+
+	// for write
+	assign SRAM_WE_N = 1'b0;
+	// cycle 1
+	assign SRAM_ADDR = {address[18:2] , 1'b0};
+	assign SRAM_DQ = write_data[15:0];
+	// cycle 2
+	assign SRAM_ADDR = {address[18:2] , 1'b1};
+	assign SRAM_DQ = write_data[31:15];
+
+	// for read 
+	
+	assign SRAM_UB_N = 1'b0;
+	assign SRAM_LB_N = 1'b0; 
+	assign SRAM_CE_N = 1'b0; 
+	assign SRAM_OE_N = 1'b0; 
+
 
 endmodule
