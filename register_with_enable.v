68794796424d81d1523d3cb71fde96bec80e7b2b
Nazanin Sabri <nazanin.sabrii@gmail.com>
2019-01-02 12:28:09 +0330

    there is a possibility it has been fixed

--- register_with_enable.v
+++ register_with_enable.v
@@ -4,9 +4,10 @@ module register_with_enable #(parameter len) (input clock,
 	input [len-1:0] input_value, 
 	output reg [len-1:0] output_value);
 
-    always @ (posedge clock) begin
-        if (reset) output_value <= 0;
-        else if(enable) output_value <= input_value;
+    always @ (posedge clock , posedge reset) begin
+      if (reset) output_value = 0;
+      else if(enable) output_value = input_value;
+		else output_value = output_value;
     end 
 
 endmodule 
